39cf989fdebed59345874d3cf74fb9dcb052a89b
Aliaksandra Safonava <alexa-09@bk.ru>
2024-03-04 18:59:00 +0300

    feat: add gqlSchema

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,7 @@
 import { Type } from '@fastify/type-provider-typebox';
+import { GraphQLSchema } from 'graphql';
+import { Query } from './query.js';
+import { Mutations } from './mutation.js';
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +21,8 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+export const gqlSchema = new GraphQLSchema({
+  query: Query,
+  mutation: Mutations,
+});
